974c49eeaeea493348b19cdd0b31d540ce54da8d
NevolinAlexandrr <nevolin_alexadnrr@mail.ru>
2023-06-22 21:51:49 +0300

    apgrade test for old and current time published

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -17,3 +17,20 @@ class QuestionModelTests(TestCase):
 		future_questions = Question(pub_date=time)
 		self.assertIs(future_questions.was_publishied_recently(), False)
 
+	def test_was_published_recently_with_old_question(self):
+		"""
+		was_publishied_recently() return False for question whose
+		pub_date is old (days and more ago)
+		"""
+		time = timezone.now() - datetime.timedelta(days=1,seconds=1)
+		old_questions = Question(pub_date=time)
+		self.assertIs(old_questions.was_publishied_recently(), False)
+	
+	def test_was_published_recently_with_recent_question(self):
+		"""
+		was_publishied_recently() return True for question whose
+		recently pub_date
+		"""
+		time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+		recent_questions = Question(pub_date=time)
+		self.assertIs(recent_questions.was_publishied_recently(), True)
